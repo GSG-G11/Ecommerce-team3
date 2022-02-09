0ada07f672c8ec95e3312f11efa0ec774f831794
ManarAbed <manarabed333@gmail.com>
2022-02-09 04:20:42 -0800

    make add to cart func with total price relates #19

---
+++ js/test.js
@@ -0,0 +1,11 @@
+const ProductNum = require('./logic.js');
+
+describe('Testing Product num return value' , ()=>{
+    test('Should return product numbers when given add to cart' , () =>{
+    const actual = ProductNum()
+    const expected = 
+        expect().toBe()
+    })
+})
+
+
